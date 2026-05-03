d38b5943274f1b67c37573d3f12d25ac096a7605
Ryan Durland <46659883+RoboRyGuy@users.noreply.github.com>
2026-05-02 22:31:47 -0700

    Need to sync

---
+++ worlds/gtfo/mid_model.py
@@ -0,0 +1,256 @@
+
+from typing import Any, List, Mapping, Optional, Type, TypeVar
+
+T = TypeVar("T")
+
+"""
+The prefix `MID_` is added to most classes in this file to prevent naming conflicts
+"""
+
+class LazyObject:
+    """Base class which is just a dict with some helper accessors."""
+
+    def __init__(self, source: Mapping[str, Any]):
+        if not isinstance(source, Mapping):
+            raise TypeError(f"LazyObject source must be a Mapping, got {type(source)}")
+        self.source = source
+
+    @classmethod
+    def convert(typ: Type[T], value: Any):
+        """
+        Helper to try and convert one value into another
+        """
+        if typ is Any:
+            return value
+        elif isinstance(typ, type) and issubclass(typ, LazyObject):
+            return typ(value)
+        try:
+            return typ(value)
+        except Exception as exc:
+            raise TypeError(
+                f"Failed to convert value {value!r} to type {typ}: {exc}"
+            ) from exc
+
+    def get_value(self, typ: Type[T], *keys: str) -> Optional[T]:
+        """
+        Look up the first matching key in `source`, then convert the value to `typ`.
+        Returns None if no key is found.
+        """
+        for key in keys:
+            if key in self.source:
+                return LazyObject.convert(type, self.source[key])
+        else:
+            raise KeyError(f"Failed to find key(s): {keys}")
+            return None
+
+    def get_list(self, type: Type[T], *keys: str) -> List[T]:
+        """
+        Look up the first matching key in `source`, then convert the value to `typ`.
+        Returns an empty list if no key is found.
+        """
+        values = None
+        for key in keys:
+            if key in self.source:
+                values = self.source[key]
+                break
+        else:
+            raise KeyError(f"Failed to find key(s): {keys}")
+            return []
+
+        result = []
+        for value in values:
+            result.append(LazyObject.convert(type, value))
+        return result
+    
+class Mid_ExpeditionData(LazyObject):
+    """Wraps data about a particular expedition"""
+
+    def get_name(self) -> str:
+        return self.get_value(str, "Name")
+
+    def get_reachable_regions(self) -> List[int]:
+        return self.get_list(int, "ReachableRegions")
+
+
+class Mid_Tag(LazyObject):
+    """Wraps an imported Randomization Tag"""
+
+    def get_id(self) -> int:
+        return self.get_value(int, "ID")
+
+    def get_name(self) -> str:
+        return self.get_value(str, "Name")
+
+    def get_description(self) -> str:
+        return self.get_value(str, "Description")
+
+    def get_description(self) -> int:
+        return self.get_value(int, "Parent")
+
+
+class Mid_Region(LazyObject):
+    """Wraps an imported region"""
+
+    def get_id(self) -> int:
+        return self.get_value(int, "ID")
+
+    def get_name(self) -> str:
+        return self.get_value(str, "Name")
+
+    def get_connected_paths(self) -> List[int]:
+        return self.get_list(int, "ConnectedPaths")
+
+    def get_connected_locations(self) -> List[int]:
+        return self.get_list(int, "ConnectedLocations")
+
+
+class Mid_ReqItem(LazyObject):
+    """
+    Wraps an imported path requirement.
+    This represents the item tag that is needed 
+     and if it's a category or item tag
+    """
+
+    def get_type(self) -> str:
+        """Currently one of 'Item', 'Category', or 'None'"""
+        return self.get_value(str, "Type")
+
+    def get_target(self) -> int:
+        return self.get_value(int, "Target")
+
+
+class Mid_Path(LazyObject):
+    """Wraps an imported path"""
+
+    def get_id(self) -> int:
+        return self.get_value(int, "ID")
+
+    def get_name(self) -> Optional[str]:
+        return self.get_value(Optional[str], "Name")
+
+    def get_starting_region(self) -> int:
+        return self.get_starting_region(int, "StartingRegion")
+
+    def get_ending_region(self) -> int:
+        return self.get_ending_region(int, "EndingRegion")
+
+    def get_req_item(self) -> Mid_ReqItem:
+        return self.get_value(Mid_ReqItem, "ReqItem")
+
+    def get_req_count(self) -> int:
+        return self.get_value(int, "ReqCount")
+
+    def get_alt_item(self) -> Mid_ReqItem:
+        return self.get_value(Mid_ReqItem, "AlternateItem")
+
+
+class Mid_LocationData(LazyObject):
+    """Location data, which describes how a location is randomized"""
+
+    def get_priority_mode(self) -> str:
+        """Currently one of 'Default', 'Priority', 'Excluded', or 'Trap'"""
+        return self.get_value(str, "PriorityMode")
+
+
+class Mid_Location(LazyObject):
+    """Wraps an imported location"""
+
+    def get_id(self) -> int:
+        return self.get_value(int, "ID")
+
+    def get_name_tag(self) -> int:
+        return self.get_value(int, "NameTag")
+
+    def get_tag2(self) -> int:
+        return self.get_value(int, "Tag2")
+
+    def get_tag3(self) -> int:
+        return self.get_value(int, "Tag3")
+
+    def get_item_id(self) -> int:
+        return self.get_value(int, "ItemID")
+
+    def get_rand_data(self) -> Mid_LocationData:
+        return self.get_value(Mid_LocationData, "RandData")
+
+class Mid_ItemData(LazyObject):
+    """Item data, which describes how an item is randomized"""
+
+    def get_is_progression(self) -> bool:
+        return self.get_value(bool, "IsProgression")
+
+    def get_is_useful(self) -> bool:
+        return self.get_value(bool, "IsUseful")
+
+    def get_is_filler(self) -> bool:
+        return self.get_value(bool, "IsFiller")
+
+    def get_is_trap(self) -> bool:
+        return self.get_value(bool, "IsTrap")
+
+    def get_do_skip_balancing(self) -> bool:
+        return self.get_value(bool, "DoSkipBalancing")
+
+    def get_is_deprioritized(self) -> bool:
+        return self.get_value(bool, "IsDeprioritized")
+
+
+class Mid_Item(LazyObject):
+    """Wraps an imported item"""
+
+    def get_id(self) -> int:
+        return self.get_value(int, "ID")
+
+    def get_name_tag(self) -> int:
+        return self.get_value(int, "NameTag")
+
+    def get_tag2(self) -> int:
+        return self.get_value(int, "Tag2")
+
+    def get_tag3(self) -> int:
+        return self.get_value(int, "Tag3")
+
+    def get_rand_data(self) -> Mid_ItemData:
+        return self.get_value(Mid_ItemData, "RandData")
+
+    def get_path_reqs(self) -> Mid_ReqItem:
+        """
+        Which pathing requirements this is meant to fulfill.
+        Typically ignored unless the type is 'Category'
+        """
+        self.get_value(Mid_ReqItem, "PathReqs")
+
+    def get_required_expedition(self) -> str:
+        """
+        Ties this item to a particular expedition.
+        Prevents it from entering the pool unless that expedition is selected.
+        """
+        return self.get_value(str, "RequiredExpeditionName")
+
+
+class Mid_GameData(LazyObject):
+    """Wraps all data imported from GTFO"""
+
+    def get_expeditions(self) -> List[Mid_ExpeditionData]: 
+        return self.get_list(str, "Expeditions")
+        
+    def get_tags(self) -> List[Mid_Tag]:
+        return self.get_list(Mid_Tag, "Tags")
+
+    def get_regions(self) -> List[Mid_Region]:
+        return self.get_list(Mid_Region, "Regions")
+
+    def get_paths(self) -> List[Mid_Path]:
+        return self.get_list(Mid_Path, "Paths")
+
+    def get_locations(self) -> List[Mid_Location]:
+        return self.get_list(Mid_Location, "Locations")
+
+    def get_items(self) -> List[Mid_Item]:
+        return self.get_list(Mid_Item, "Items")
+
+    def get_floating_items(self) -> List[int]:
+        return self.get_list(int, "FloatingItems")
+    
+
+
--- worlds/gtfo/options.py
+++ worlds/gtfo/options.py
@@ -1,104 +1,52 @@
-from dataclasses import dataclass
-import json
 from typing import Any, Dict
+from Options import *
 
-from Options import Choice, DeathLink, DefaultOnToggle, ExcludeLocations, NamedRange, OptionDict, \
-    OptionGroup, PerGameCommonOptions, Range, Removed, Toggle, ItemSet
+## GTFO Game Options
 
-## Game Options
+class RequiredExpeditions(OptionSet):
+    """
+    The expeditions to use for randomization, specified by name.
+    The special name \"ALL\" can be used to select all available
+     expeditions, though this is not recommended.
+    The expeditions will be hinted to appear in the order listed.
+    At least one expedition must be specified.
+    """
+    display_name = "Required Expeditions List"
+    default = set([ "R1B1", "R1A1", "R8B2", "R1C1", "R8D1", "R8E1"])
+    verify_item_name = False
+    verify_location_name = False
 
-gtfo_big_number = 99999
-"""An arbitrarily large value"""
+class RequireSecondaries(Toggle):
+    """
+    If true, winning requires clearing secondary (as well as main) on all selected expeditions
+    """
+    display_name = "Require Secondaries"
 
-class RequiredExpeditions(ItemSet):
+class RequireOverloads(Toggle):
     """
-    The expeditions required for completion. Up to four of each tier.
-    All other expeditions will be removed from the game.
-    Items and locations will randomize only into the expeditions listed here.
-    Use the name as found in the level's description (before pressing "Host Lobby")
+    If true, winning requires clearing overload (as well as main) on all selected expeditions
     """
-    default = set([ "R1A1", "R1B1", "R8B2", "R1C1", "R8D1", "R8E1"])
+    display_name = "Require Overloads"
 
-class RequireAllObjectives(Toggle):
+class RandomizationWhitelist(OptionSet):
     """
-    If true, the goal is to complete main, secondary, overload, and PE on all expeditions listed in 
-    required_expeditions.
-    If false, you only need to complete main on all listed expeditions.
+    Items and locations matching this whitelist will attempt to randomize.
+    Nothing randomizes by default; it must be whitelisted.
     """
+    display_name = "Randomization Whitelist"
 
-class LockExpeditions(DefaultOnToggle):
+class RandomizationBlacklist(OptionSet):
     """
-    Start with all levels locked except for the first one listed in required_expeditions.
-    The level unlocks will be added as items to the multiworld.
+    Items and locations matching this blacklist will not randomize.
+    Blacklist takes priority over the whitelist
     """
+    display_name = "Randomization Blacklist"
 
-class LockGear(DefaultOnToggle):
+class EarlyItems(OptionList):
     """
-    Start with all gear locked.
-    Gear unlocks will be added as items to the multiworld.
-    Make sure to add entries to starting_gear to allow progression.
+    One item from each tag specified in this list will be added to first sphere items.
     """
-
-class StartingGear(ItemSet):
-    """
-    Gear that will start unlocked. Only valid if lock_gear is enabled.
-    The name of the gear is the weapon's make and model name, case insensitive.
-    For example, the "Pistol" is "Shelling S49", or "shelling s49", or "SHELLING S49".
-    The default list is a bat, the burst pistol, the short rifle, and the bio tracker.
-    """
-    default = set(["Kovac Peacekeeper", "Shelling Nano", "Drekker CLR", "D-Tek Optron IV"])
-
-class LockPlayerSlots(NamedRange):
-    """
-    Lock some or all of the lobby slots, preventing bots and humans from joining.
-    Any number higher than the max (3 by default, 7 when using LobbyExapansion) will act as additional filler unlocks.
-    """
-    range_start = 0
-    range_end = gtfo_big_number
-    special_range_names = {
-        "None": 0,
-        "False": 0,
-        "True": 7,
-    }
-    default = 0
-
-class FillerCount(Range):
-    """
-    How many filler items to add. The more the merrier!
-    """
-    range_start = 0
-    range_end = gtfo_big_number
-    default = 100
-
-class TrapPercent(NamedRange):
-    """
-    The percentage of filler items which will be traps.
-    """
-    range_start = 0
-    range_end = 1
-    special_range_names = {
-        "None": 0,
-        "False": 0,
-        "True": 1,
-    }
-    default = 0.5
-
-class TrapWeight(Range):
-    """
-    Used when generating traps to determine how bad the traps are.
-    Higher values result in dangerous traps being more common.
-    Lower values result in less dangerous traps being more common.
-    """
-    range_start = 0
-    range_end = 1
-    special_range_names = {
-        "Min": 0,
-        "Low": 0.2,
-        "Medium": 0.5,
-        "High": 0.8,
-        "Max": 1,
-    }
-    default = 0.5
+    display_name = "Early Items"
 
 
 @dataclass
@@ -106,29 +54,21 @@ class GTFOOptions(PerGameCommonOptions):
     """Configuration options for GTFO randomization"""
 
     required_expeditions: RequiredExpeditions
-    require_all_objectives: RequireAllObjectives
-    lock_expeditions: LockExpeditions
-    lock_gear: LockGear
-    starting_gear: StartingGear
-    lock_player_slots: LockPlayerSlots
-    filler_count: FillerCount
-    trap_precent: TrapPercent
-    trap_weight: TrapWeight
+    require_secondaries: RequireSecondaries
+    require_overloads: RequireOverloads
+    whitelist: RandomizationWhitelist
+    blacklist: RandomizationBlacklist
+    early_items: EarlyItems
 
 
 gtfo_option_groups = [
-    OptionGroup("Expeditions", [
+    OptionGroup("Main Conditions", [
         RequiredExpeditions,
-        LockExpeditions
+        RequireSecondaries,
+        RequireOverloads
     ]),
-    OptionGroup("Gear", [
-        LockGear,
-        StartingGear,
-        LockPlayerSlots,
-    ]),
-    OptionGroup("Filler", [
-        FillerCount,
-        TrapPercent,
-        TrapWeight,
+    OptionGroup("Randomization", [
+        RandomizationWhitelist,
+        RandomizationBlacklist,
     ]),
 ]
